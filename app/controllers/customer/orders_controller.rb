d921edeb07a0803ea1e0c44052e647e49bfdea80
unknown <grenson.crtis@gmail.com>
2020-06-18 14:44:08 +0900

    [add]order

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -8,6 +8,27 @@ class Customer::OrdersController < ApplicationController
   end
 
   def confirm
+    @tax = 1.1
+    @postage = 800
+    @total_price = 0
+
+    @cart_items = CartItem.where(end_user: current_end_user)
+    @pay_type = order_params[:pay_type]
+    if order_params[:address_type] == "1"
+      @postcode = current_end_user.postcode
+      @address = current_end_user.address
+      @direction = current_end_user.first_name + current_end_user.last_name
+    elsif order_params[:address_type] == "2"
+      # findでdelivery_idの情報を全部取得
+      @delivery = Delivery.find(order_params[:delivery_id])
+      @postcode = @delivery.postcode
+      @address = @delivery.address
+      @direction = @delivery.direction
+    else
+      @postcode = order_params[:postcode]
+      @address = order_params[:address]
+      @direction = order_params[:direction]
+    end
   end
 
   def thanks
@@ -18,7 +39,13 @@ class Customer::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:address_type)
+    params.require(:order).permit(:pay_type,
+                                  :address_type,
+                                  :delivery_id,
+                                  :postcode,
+                                  :address,
+                                  :direction
+                                  )
   end
   # end_user_id
 end
